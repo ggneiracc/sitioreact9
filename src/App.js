5a71d887752d0945ff371841c21b9d27def8c50a
Gonzalo Neira Calsin <ggneiracc68@gmail.com>
2022-10-26 14:04:54 -0500

    V05 my-app3. CRUD Básico completo, aunque hay una falla... No actualiza en tiempo real

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
 //import logo from './logo.svg';
 //import './App.css';
-import { GuardarRegistroPersona } from './componente/api';
+import { borrarRegistro, GuardarRegistroPersona, reportePersonas, updatePerson } from './componente/api';
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { async } from '@firebase/util';
 
 function App() {
   const [nombrePersona, setNombrePersona] = useState(null);
+  const [idPersona, setIdPersona] = useState(null);
+  const [persona, setPersona] = useState(null);
 
-  const GuardarPersona = () => {
-    GuardarRegistroPersona(nombrePersona);
-    console.log(nombrePersona);
+  const GuardarPersona = async () => {
+    await GuardarRegistroPersona(nombrePersona);
+    listaPersonas();
+    //console.log(nombrePersona);
   }
 
+  const  removeRegistro = async () => {
+    await borrarRegistro(idPersona);
+    listaPersonas();
+  }
+
+  useEffect( () => {
+    listaPersonas();
+  }, [])
+
+  const listaPersonas = async () => {
+    const p = await reportePersonas();
+    setPersona(p.docs);
+    //console.log(p.docs[0].data());  
+  }
+
+  const updatePersonData = async () =>{
+    await updatePerson(idPersona, nombrePersona);
+    listaPersonas();
+  }
+ 
   return (
     <div className="App">
-      <input type="text" onChange={ e => setNombrePersona(e.target.value)} /> 
+      <input type="text" onChange={ e => setNombrePersona(e.target.value)} placeholder="Nombres completos" /> 
+      <input type="text" onChange={ e => setIdPersona(e.target.value)} placeholder="Código de persona" /> 
+
       <button onClick={GuardarPersona}>Guardar</button>
+      <button onClick={removeRegistro}>Eliminar</button>
+      <button onClick={updatePersonData}>Update</button>
+
+      {
+        persona && persona.map( p => <p>{p.id} - { p.data().name }</p>) 
+      }
     </div>
   );
 }
--- src/componente/api.js
+++ src/componente/api.js
@@ -3,13 +3,33 @@ import { collection, getDocs, query, doc, getDoc, addDoc, deleteDoc, updateDoc, 
 
 
 import React from 'react'       //rafce     //sfc
+import { async } from '@firebase/util';
 
+//REGISTRAR
 export const GuardarRegistroPersona = (name) => {
     //obj = { name } Ejemplo //personas es coleccion de firebase
     addDoc(collection(db, 'personas'), { name });         
 }
 
-//export default GuardarRegistroPersona;
+//REPORTES
+export const reportePersonas = async () => {
+    const result = await getDocs(query(collection(db, 'personas')));
+    return result;
+}
+ 
+//DELETE
+export const borrarRegistro = async (id) => {
+    await deleteDoc(doc(db, 'personas', id));
+}
+ 
+
+//UPDATE
+export const updatePerson = async (id, name) => {
+    await await updateDoc(doc(db, 'personas', id), { name });;
+}
+ 
+
+
 
 //ALTA
 //addDoc(collection(db, 'items'), obj);         //obj = { name } Ejemplo
